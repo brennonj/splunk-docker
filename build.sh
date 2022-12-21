2e69e1a8ac7eca1624ebf842b38c25bcd1afdde7
Brennon Jacobson <brennonj@me.com>
2022-12-21 09:55:50 -0800

    Update Docker for Latest Compose

--- build.sh
+++
@@ -1,9 +0,0 @@
-#https://docs.docker.com/engine/reference/commandline/build/
-if [ -z $CURRENT ]; then
-	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-fi
-
-docker build --no-cache=true -t splunk/splunk:latest .
-docker tag splunk/splunk:latest splunk/splunk:latest 
-docker tag splunk/splunk:latest registry.splunk.com/splunk/splunk:latest 
-docker tag splunk/splunk:latest registry.splunk.com/splunk/splunk:latest
